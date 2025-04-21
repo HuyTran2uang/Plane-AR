e3a66fd5ac07ba952a0779dd39e4f510bbedd193
Tran Quang Huy <86909150+HuyTran2uang@users.noreply.github.com>
2025-04-21 23:12:16 +0700

    update UX UI and show stats

--- Assets/_Main/Scripts/Plane.cs
+++ Assets/_Main/Scripts/Plane.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using DG.Tweening;
+using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -11,12 +12,54 @@ public class Plane : MonoBehaviour
     //view
     public TMP_Dropdown dropdown;
     //info
+    [Header("Info")]
     public TMP_Text f_dayTXT;
     public TMP_Text f_canTXT;
     public TMP_Text f_nangTXT;
     public TMP_Text pTXT;
     public TMP_Text v_duoiTXT;
     public TMP_Text v_trenTXT;
+    //
+    [Header("Button")]
+    public Button _arrowBtn;
+    public Button _infoBtn;
+    public Button _fBtn;
+    public Button _vBtn;
+    public Button _tBtn;
+    [Header("Component")]
+    public GameObject _info;
+    public List<GameObject> _listF;
+    public List<GameObject> _listV;
+    public List<GameObject> _listT;
+    //arrow
+    public RectTransform _listBtns;
+    public Image _iconArrow;
+    private bool isShowingListBtns = true;
+    private bool isSwitching = false;
+
+    private void Awake()
+    {
+        _arrowBtn.onClick.AddListener(() =>
+        {
+            DisplayListBtns();
+        });
+        _infoBtn.onClick.AddListener(() =>
+        {
+            _info.SetActive(!_info.activeSelf);
+        });
+        _fBtn.onClick.AddListener(() =>
+        {
+            _listF.ForEach(i => i.SetActive(!i.activeSelf));
+        });
+        _vBtn.onClick.AddListener(() =>
+        {
+            _listV.ForEach(i => i.SetActive(!i.activeSelf));
+        });
+        _tBtn.onClick.AddListener(() =>
+        {
+            _listT.ForEach(i => i.SetActive(!i.activeSelf));
+        });
+    }
 
     private void Start()
     {
@@ -38,11 +81,36 @@ public class Plane : MonoBehaviour
 
     public void ShowStats(StatSO stats)
     {
-        f_dayTXT.SetText($"F đẩy: {stats.F_day}");
-        f_canTXT.SetText($"F cản: {stats.F_can}");
-        f_nangTXT.SetText($"F nâng: {stats.F_nang}");
-        pTXT.SetText($"P: {stats.P}");
-        v_duoiTXT.SetText($"V dưới: {stats.V_duoi}");
-        v_trenTXT.SetText($"V trên: {stats.V_tren}");
+        f_dayTXT.SetText($"F đẩy: {stats.F_day} N");
+        f_canTXT.SetText($"F cản: {stats.F_can} N");
+        f_nangTXT.SetText($"F nâng: {stats.F_nang} N");
+        pTXT.SetText($"P: {stats.P} N");
+        v_duoiTXT.SetText($"V dưới: {stats.V_duoi} km/h");
+        v_trenTXT.SetText($"V trên: {stats.V_tren} km/h");
+    }
+
+    private void DisplayListBtns()
+    {
+        if (isSwitching) return;
+        isSwitching = true;
+        //420
+        if (isShowingListBtns)
+        {
+            _iconArrow.transform.localScale = new Vector3(1, -1, 1);
+            _listBtns.DOSizeDelta(new Vector2(100, 0), .5f).OnComplete(() =>
+            {
+                isSwitching = false;
+                isShowingListBtns = false;
+            });
+        }
+        else
+        {
+            _iconArrow.transform.localScale = new Vector3(1, 1, 1);
+            _listBtns.DOSizeDelta(new Vector2(100, 420), .5f).OnComplete(() =>
+            {
+                isSwitching = false;
+                isShowingListBtns = true;
+            });
+        }
     }
 }
